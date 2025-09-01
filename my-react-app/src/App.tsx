8321a894abcf38bdfc7eb368d8531ab338c10415
Lara Ismael <lara8ismael@gmail.com>
2025-09-01 12:50:20 -0700

    basic structure of tic-tac-toe

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,27 +1,44 @@
-import "./App.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => console.log(`count has changed to: $(count)`), [count]);
-
-  const increment = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
-
-  const decrement = () => {
-    setCount((prevCount) => prevCount - 1);
-  };
-
+function Square({
+  value,
+  onSquareClick,
+}: {
+  value: string;
+  onSquareClick: () => void;
+}) {
   return (
-    <>
-      <h1>React simple Counter</h1>
-      <h3>Counter: {count}</h3>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-    </>
+    <button onClick={onSquareClick} className="square">
+      {value}
+    </button>
   );
 }
 
-export default App;
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(""));
+
+  function onSquareClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onClick={onSquareClick} />
+        <Square value={squares[1]} onClick={onSquareClick} />
+        <Square value={squares[2]} onClick={onSquareClick} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onClick={onSquareClick} />
+        <Square value={squares[4]} onClick={onSquareClick} />
+        <Square value={squares[5]} onClick={onSquareClick} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onClick={onSquareClick} />
+        <Square value={squares[7]} onClick={onSquareClick} />
+        <Square value={squares[8]} onClick={onSquareClick} />
+      </div>
+    </>
+  );
+}
